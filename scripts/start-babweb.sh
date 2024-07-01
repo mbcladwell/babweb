b2281d0da1e78615ddb6c1a50caec77e77d1f897
mbcladwell <mbcladwell@gmail.com>
2024-07-01 15:01:29 -0400

    autocommit

--- scripts/start-babweb.sh
+++ scripts/start-babweb.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PATH_INTO_STORE=babwebstorepath
 export LC_ALL="C"
+export GUILE_LOAD_PATH=guileloadpath
+export GUILE_LOAD_COMPILED_PATH=guileloadcompiledpath
 mkdir -p /var/tmp/babweb/tmp/cache
 cd  $PATH_INTO_STORE/share/guile/site/3.0/babweb
 art.in work -h0.0.0.0 --config=$HOME/.config/babweb/artanis.conf
