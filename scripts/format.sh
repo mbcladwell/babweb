30f1380005f9d17dda6b88d2569d3838bbb0f2c3
mbcladwell <mbcladwell@gmail.com>
2024-07-01 04:57:51 -0400

    autocommit

---
+++ scripts/format.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+export LC_ALL="C"
+export GUILE_LOAD_PATH=guileloadpath
+export GUILE_LOAD_COMPILED_PATH=guileloadcompiledpath
+guileexecutable -L . -e '(babweb format)' -s babwebstorepath/share/guile/site/3.0/babweb/format.scm $1 $2
+
---
+++ scripts/init-acct.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+export LC_ALL="C"
+export GUILE_LOAD_PATH=guileloadpath
+export GUILE_LOAD_COMPILED_PATH=guileloadcompiledpath
+guileexecutable -e '(babweb initacct)' -s babwebstorepath/share/guile/site/3.0/babweb/initacct.scm $1 $2 $3
